8693f008033a001e9843f8c9885ef1ceb2d6730e
kevinshen56714 <kshen64@gatech.edu>
2021-07-21 11:06:30 -0400

    heroku config fix

--- src/server/index.ts
+++ src/server/index.ts
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('dist'))
+// app.use(express.static('dist'))
 
 const server = http.createServer(app)
 const gameServer = new Server({
