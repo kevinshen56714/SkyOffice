de8208cd1aab343405586d3debb2c4c5e75dc0e6
kevinshen56714 <kshen64@gatech.edu>
2021-10-02 22:02:53 -0400

    rm server/lib

--- server/lib/types/Messages.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Message = void 0;
-var Message;
-(function (Message) {
-    Message[Message["UPDATE_PLAYER"] = 0] = "UPDATE_PLAYER";
-    Message[Message["READY_TO_CONNECT"] = 1] = "READY_TO_CONNECT";
-    Message[Message["DISCONNECT_STREAM"] = 2] = "DISCONNECT_STREAM";
-})(Message = exports.Message || (exports.Message = {}));
