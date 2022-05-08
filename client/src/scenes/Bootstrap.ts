fbfe52129d5318324724fa162ba9e9a3b10ebd36
kevinshen56714 <kshen64@gatech.edu>
2022-05-07 21:36:59 -0400

    Move item images from public to src

--- client/src/scenes/Bootstrap.ts
+++ client/src/scenes/Bootstrap.ts
@@ -3,6 +3,10 @@ import Network from '../services/Network'
 import { BackgroundMode } from '../../../types/BackgroundMode'
 import store from '../stores'
 import { setRoomJoined } from '../stores/RoomStore'
+import ChairImg from '../images/items/chair.png'
+import ComputerImg from '../images/items/computer.png'
+import VendingMachineImg from '../images/items/vendingmachine.png'
+import WhiteboardImg from '../images/items/whiteboard.png'
 
 export default class Bootstrap extends Phaser.Scene {
   network!: Network
@@ -31,31 +35,15 @@ export default class Bootstrap extends Phaser.Scene {
       frameWidth: 32,
       frameHeight: 32,
     })
-    this.load.spritesheet('chairs', 'assets/items/chair.png', {
-      frameWidth: 32,
-      frameHeight: 64,
-    })
-    this.load.spritesheet('computers', 'assets/items/computer.png', {
-      frameWidth: 96,
-      frameHeight: 64,
-    })
-    this.load.spritesheet('whiteboards', 'assets/items/whiteboard.png', {
-      frameWidth: 64,
-      frameHeight: 64,
-    })
-    this.load.spritesheet('vendingmachines', 'assets/items/vendingmachine.png', {
-      frameWidth: 48,
-      frameHeight: 72,
-    })
-    this.load.spritesheet('office', 'assets/items/Modern_Office_Black_Shadow.png', {
+    this.load.spritesheet('office', 'assets/tileset/Modern_Office_Black_Shadow.png', {
       frameWidth: 32,
       frameHeight: 32,
     })
-    this.load.spritesheet('basement', 'assets/items/Basement.png', {
+    this.load.spritesheet('basement', 'assets/tileset/Basement.png', {
       frameWidth: 32,
       frameHeight: 32,
     })
-    this.load.spritesheet('generic', 'assets/items/Generic.png', {
+    this.load.spritesheet('generic', 'assets/tileset/Generic.png', {
       frameWidth: 32,
       frameHeight: 32,
     })
@@ -74,6 +62,22 @@ export default class Bootstrap extends Phaser.Scene {
     this.load.spritesheet('nancy', 'assets/character/nancy.png', {
       frameWidth: 32,
       frameHeight: 48,
+    })
+    this.load.spritesheet('chairs', ChairImg, {
+      frameWidth: 32,
+      frameHeight: 64,
+    })
+    this.load.spritesheet('computers', ComputerImg, {
+      frameWidth: 96,
+      frameHeight: 64,
+    })
+    this.load.spritesheet('whiteboards', WhiteboardImg, {
+      frameWidth: 64,
+      frameHeight: 64,
+    })
+    this.load.spritesheet('vendingmachines', VendingMachineImg, {
+      frameWidth: 48,
+      frameHeight: 72,
     })
   }
 
