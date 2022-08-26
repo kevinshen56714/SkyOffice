f0a1e6206c3e6108a3dd79d41b8bc1bd8b9c1cbd
kevinshen56714 <kshen64@gatech.edu>
2022-08-25 21:52:48 -0400

    test

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,9 +1,2 @@
 /// <reference types="vite/client" />
 declare module '*.png'
-
-declare namespace Intl {
-  interface DateTimeFormatOptions {
-    dateStyle?: 'full' | 'long' | 'medium' | 'short'
-    timeStyle?: 'full' | 'long' | 'medium' | 'short'
-  }
-}
