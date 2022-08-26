32a7b6d291a5162dc6dc3b83b3a760e813061ee4
kevinshen56714 <kshen64@gatech.edu>
2022-08-25 23:10:04 -0400

    Change back to main server

--- client/src/services/Network.ts
+++ client/src/services/Network.ts
@@ -33,7 +33,7 @@ export default class Network {
     const protocol = window.location.protocol.replace('http', 'ws')
     const endpoint =
       process.env.NODE_ENV === 'production'
-        ? `wss://sky-office-dev.herokuapp.com/`
+        ? `wss://sky-office.herokuapp.com/`
         : `${protocol}//${window.location.hostname}:2567`
     this.client = new Client(endpoint)
     this.joinLobbyRoom().then(() => {
