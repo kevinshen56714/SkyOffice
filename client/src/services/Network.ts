1bb5d57f03bea01a27549d5e1d827a174c9760dc
kevinshen56714 <kshen64@gatech.edu>
2022-12-03 09:28:06 -0500

    Embrace Heroku again and put server URL in .env

--- client/src/services/Network.ts
+++ client/src/services/Network.ts
@@ -33,7 +33,7 @@ export default class Network {
     const protocol = window.location.protocol.replace('http', 'ws')
     const endpoint =
       process.env.NODE_ENV === 'production'
-        ? `wss://skyoffice.onrender.com/`
+        ? process.env.REACT_APP_SERVER_URL
         : `${protocol}//${window.location.hostname}:2567`
     this.client = new Client(endpoint)
     this.joinLobbyRoom().then(() => {
