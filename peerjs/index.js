bf539362c08198fc351150b6ea9b0c246ed99b2d
Stefan Nachtrab <stefan.nachtrab@adorsys.com>
2023-10-20 15:01:49 +0200

    fixed temporary dev state

--- peerjs/index.js
+++ peerjs/index.js
@@ -4,7 +4,6 @@ const port = process.env.PEER_PORT || 9000
 const path = process.env.PEER_PATH || '/'
 const secret = process.env.PEER_SECRET || 'topsecret'
 
-const peerServer = PeerServer({ port: port, path: path, secret: secret })
+const peerServer = PeerServer({ port: port, path: path, key: secret })
 
 console.log(`PeerServer started on host ${port} with path ${path}`)
-docker run --restart=always --name peerjs -d -p 9000:9000 -e PEER_PORT=9000 -e PEER_SECRET=1234567890 -e PEER_PATH=/peerjs st3v0rr/peerjs
