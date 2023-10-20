5377b4591298647c1d9fdb591c09c6aec8fc1303
Stefan Nachtrab <stefan.nachtrab@adorsys.com>
2023-10-20 15:33:46 +0200

    fixed key connection issue

--- peerjs/index.js
+++ peerjs/index.js
@@ -1,9 +1,15 @@
 const { PeerServer } = require('peer')
 
 const port = process.env.PEER_PORT || 9000
+const proxied = process.env.PEER_PROXIED || false
 const path = process.env.PEER_PATH || '/'
-const secret = process.env.PEER_SECRET || 'topsecret'
+const secret = process.env.PEER_SECRET || 'peerjs'
 
-const peerServer = PeerServer({ port: port, path: path, key: secret })
-
+const peerServer = PeerServer({ port: port, path: path, key: secret, proxied: proxied })
+peerServer.on('connection', (client) => {
+  console.log('Client connected', client)
+})
+peerServer.on('disconnect', (client) => {
+  console.log('Client disconnected', client)
+})
 console.log(`PeerServer started on host ${port} with path ${path}`)
